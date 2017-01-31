274b9a3f2c636063e76ab4dc60d0749aaa43eb27
alumno <alumno@328-06>
2017-01-31 10:55:33 -0600

    Programa listo

--- src/spp/ncortesd/ejemplomenu/SPPNCortesDEjemploMenu.java
+++ src/spp/ncortesd/ejemplomenu/SPPNCortesDEjemploMenu.java
@@ -32,34 +32,39 @@ public class SPPNCortesDEjemploMenu {
         switch (opEntrada){
             case 1:
                 System.out.println("Bienvenido al cálculo de áreas");
-                System.out.println("Introduce el lado del cuadrado");
-                lado=kb.nextDouble();
+                //Llamamos el método para solicitar datos
+                //Lo que devuelve lo guardamos en la variable lado
+                lado= mensajePedirdatos();
                 resultado= Math.pow(lado,2);
                 System.out.println("El área del cuadrado es "+ resultado);
                 break;
             case 2:
                 System.out.println("Bienvenido al cálculo de perímetros");
-                System.out.println("Introduce el lado del cuadrado");
-                lado =kb.nextDouble();
+                lado= mensajePedirdatos();
                 resultado= lado*4;
                 System.out.println("El resultado del perímetro es " + resultado);
                 break;
             case 3:
                 System.out.println("Bienvenido al cálculo de volúmenes");
-                System.out.println("Introduce el lado del cuadrado");
-                lado= kb.nextDouble();
-                resultado= Math.pow(lado,3);
+                //Podemos omitir la variable, sustituyendo el valor de lado por 
+                //lo de static double
+                resultado= Math.pow(mensajePedirdatos(),3);
                 System.out.println("El resultado del volumen es "+ resultado);
                 break;
             default:
                 System.out.println("Adiós");
        
         }
-        
-        
-        
-               
-        
+                         
+    }
+    
+    static double mensajePedirdatos (){
+        //Variables
+        double lado;
+        Scanner kb= new Scanner (System.in);
+        System.out.println("Introduce el lado del cuadrado");
+        lado= kb.nextDouble();
+        return lado;
     }
     
 }
